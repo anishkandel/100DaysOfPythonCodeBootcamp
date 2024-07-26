db3f5d60283776dce1c1cf02a0eef3599880bc28
anishkandel28 <anishshreekandel@gmail.com>
2024-07-26 23:26:18 +0545

    Day1

---
+++ Day1/printing_start.py
@@ -0,0 +1,57 @@
+# Write your code below this line 👇
+print ("to daa moon !")
+
+print ("it's day1 of days 100")
+
+#print () function
+
+# when the text is in double quotes, it is called a string
+# (parenthesis) is used to tell the computer that it is a string
+# syntax highligting is used to tell the computer that it is a string
+
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print('she said me :"Hello! Baby"')
+
+print("I said her:'what are you doing here'")
+
+print("She said me: \"I am here to learn python with you\" and she smiles again said \'I love you!\'")
+
+print ("I said:\"how are you\" and he said to me :\'he is good\'")
+
+print("She said: \"Hello\" and then left.")
+
+# String Manipulation
+
+#inline code
+print ("Hello world!\n Hello world \n My name is Anish Shree \"Kandel\"")
+
+# concateinate +
+
+print ("My name is " + "Anish Shree Kandel") #space before is
+print ("My name is" +" Anish shree Kandel") #space before Anish
+print ("My name is" + " "+ "Anish Shree Kandel") # + " " +
+
+#in python spaces are really important
+#it will show Identation Error, so start of beagining of the line of code.
+
+
+#***************** The Python Input function********
+
+# input () function
+# input ("A prompt for the user")
+#input() will get user input in console
+#then print() will print the word "Hello" and the user input "What is ur name?"
+print("Hi! " + input('What is your name?') + "!" + " How are you?")
+
+# =int(input())
+# num2=int(input())
+
+# print(num1*num2)
+# print(num1+num2)
+# print(num1/num2)
+# print(num1//num2)
+
---
+++ Day1/variables_start.py
@@ -0,0 +1,10 @@
+name= input("What is your name?") #here name is variable, where the data i will give to the input prompt will be stored in name
+
+print ("My name is " + name)
+
+
+name= "Don hu ma"
+print(name)
+
+length=len(name)
+print("the length of the name is " + str(length))
